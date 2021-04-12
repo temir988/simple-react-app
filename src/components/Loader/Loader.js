71352e2ab66be135dbc03d13012663433ce10f6b
Temirlan <bamblebee988@gmail.com>
2021-04-12 18:10:09 +0600

    Add loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,22 @@
+import styles from "./Loader.module.css";
+
+function Loader() {
+  return (
+    <div className={styles.spinner}>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
+
+export default Loader;
